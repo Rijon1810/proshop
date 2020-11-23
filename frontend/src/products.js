10363814cc559b68bfdc19148e3d2e14593f1761
Rijon <rijon.r@yahoo.com>
2020-11-23 23:12:09 +0600

    Frontend startted

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+    {
+        _id: '1',
+        name : 'Airpods Wireless Bluetooth Headphones',
+        image : '/images/airpods.jpg',
+        description :
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, iste reiciendis. Inventore dolor quo architecto eos pariatur temporibus, molestiae placeat delectus vel numquam iste! Asperiores enim totam dolor alias natus!',
+        brand : 'Apple',
+        category :" 'Electronics",
+        price : 89.99,
+        countInStock : 0,
+        rating : 4.5,
+        numReviews : 4
+    },
+    {
+        _id: '2',
+        name : 'iPhone 11 Pro 256GB Memory',
+        image : '/images/phone.jpg',
+        description :
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, iste reiciendis. Inventore dolor quo architecto eos pariatur temporibus, molestiae placeat delectus vel numquam iste! Asperiores enim totam dolor alias natus!',
+        brand : 'Apple',
+        category :" 'Electronics",
+        price : 599.99,
+        countInStock : 10,
+        rating : 4.0,
+        numReviews : 4
+    },
+    {
+        _id: '3',
+        name : 'Airpods Wireless Bluetooth Headphones',
+        image : '/images/airpods.jpg',
+        description :
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, iste reiciendis. Inventore dolor quo architecto eos pariatur temporibus, molestiae placeat delectus vel numquam iste! Asperiores enim totam dolor alias natus!',
+        brand : 'Apple',
+        category :" 'Electronics",
+        price : 89.99,
+        countInStock : 3,
+        rating : 4.5,
+        numReviews : 4
+    },
+    {
+        _id: '4',
+        name : 'Airpods Wireless Bluetooth Headphones',
+        image : '/images/airpods.jpg',
+        description :
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, iste reiciendis. Inventore dolor quo architecto eos pariatur temporibus, molestiae placeat delectus vel numquam iste! Asperiores enim totam dolor alias natus!',
+        brand : 'Apple',
+        category :" 'Electronics",
+        price : 89.99,
+        countInStock : 3,
+        rating : 4.5,
+        numReviews : 4
+    },
+    {
+        _id: '5',
+        name : 'Airpods Wireless Bluetooth Headphones',
+        image : '/images/airpods.jpg',
+        description :
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, iste reiciendis. Inventore dolor quo architecto eos pariatur temporibus, molestiae placeat delectus vel numquam iste! Asperiores enim totam dolor alias natus!',
+        brand : 'Apple',
+        category :" 'Electronics",
+        price : 89.99,
+        countInStock : 3,
+        rating : 4.5,
+        numReviews : 4
+    },
+    {
+        _id: '6',
+        name : 'Airpods Wireless Bluetooth Headphones',
+        image : '/images/airpods.jpg',
+        description :
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, iste reiciendis. Inventore dolor quo architecto eos pariatur temporibus, molestiae placeat delectus vel numquam iste! Asperiores enim totam dolor alias natus!',
+        brand : 'Apple',
+        category :" 'Electronics",
+        price : 89.99,
+        countInStock : 3,
+        rating : 4.5,
+        numReviews : 4
+    }
+ 
+]
+export default products;
